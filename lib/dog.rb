bc503d42159acf024aa3e3c342047a6bd88ab2fb
Austin Johnson <technicallyaustin@gmail.com>
2020-01-13 22:12:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,28 @@
 class Dog 
 @@all = []
+attr_accessor :name 
+def initialize(name)
+  @name = name
+  @@all << @name 
+  save 
+end
 
-  
-  
+def self.all
+@@all
+end
+
+def self.clear_all
+  @@all.map { |i| @@all.pop(i)}
+end
+
+def self.print_all
+  @all.map {|i| puts i}
+end
+
+def save
+  @all << self 
+end
+
+def 
   
 end
